c8499f8988001701df622885e01d38fc73f7e919
Nat Silprasert <natsilprasert@gmail.com>
2025-09-06 23:34:15 +0700

    Time: 0 ms (100.00%) | Memory: 4.1 MB (10.39%) - LeetSync

---
+++ 231-power-of-two/power-of-two.go
@@ -0,0 +1,9 @@
+func isPowerOfTwo(n int) bool {
+    if n <= 0 {return false}
+    if n == 1 {return true}
+    for n != 1 {
+        if n % 2 == 1 {return false}
+        n /= 2
+    }
+    return true
+}
